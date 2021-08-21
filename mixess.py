ba07d5b389848eaa188d55396aab78fe7ed9c33d
Discord:  m1ke#0003 <87667679+m1ke-adams@users.noreply.github.com>
2021-08-21 13:57:42 +0300

    mixess.py

--- mixess.py
+++ mixess.py
@@ -9,10 +9,12 @@ print("""
 """)
 urli=input("xss açıklı sitenin url giriniz: ")
 header={"Cookie": "security=low; PHPSESSID=a0b29e43b154e8cf261c3a93686bdd94"}
-xssl=["hacked","<h1>siber","<script>alert('hacked')</script>"]
+xssl=["cookie","<h1>cookie","<script>alert('cookie')</script>"]
 for payload in xssl:
     print(payload)
     url=urli+str(payload)
     sonuc=requests.get(url=url,headers=header)
     if str(payload) in str(sonuc.content):
         print("xss var, payloadlar: ",str(payload))
+    else:
+        print("HATA!, xss bulunamadı. Lütfen daha sonra tekrar deneyiniz.")
